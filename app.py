5e022092a950acd64d23f42c1bb4a0c332daf8ce
payalnehra36718-ux <payalnehra36718@iisuniv.ac.in>
2026-05-06 11:56:03 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 from sentence_transformers import SentenceTransformer
 from sklearn.metrics.pairwise import cosine_similarity
 
-model = SentenceTransformer('all-MiniLM-L6-v2')
+model = SentenceTransformer('all-mpnet-base-v2')
 
 st.title("Question Similarity Checker")
 st.markdown("### Compare two questions using AI embeddings")
